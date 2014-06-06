9c9c32bae3d069c7732bfb91f4da59be2d562731
kurata1016 <kurata1016@gmail.com>
2014-06-06 14:26:53 +0900

    add button

--- PianoTouchSample/src/jp/co/techfun/pianotouch/PianoTouchSampleActivity.java
+++ PianoTouchSample/src/jp/co/techfun/pianotouch/PianoTouchSampleActivity.java
@@ -6,8 +6,12 @@ import android.content.res.TypedArray;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 // ピアノ画面Activity
 @SuppressLint("Recycle")
@@ -17,6 +21,11 @@ public class PianoTouchSampleActivity extends Activity {
     private static final int WHITE_KEY_NUMBER = 12;
     // 黒鍵鍵盤数を定義する定数
     private static final int BLACK_KEY_NUMBER = 8;
+    // ボタンタグ定数
+    private static final String REC = "record";
+    private static final String PLAY = "play";
+    private static final String PAUSE = "pause";
+    private static final String STOP = "stop";
 
     // 白鍵・黒鍵音声再生用MediaPlayer配列の初期化
     private MediaPlayer[] whiteKeyPlayer;
@@ -85,6 +94,55 @@ public class PianoTouchSampleActivity extends Activity {
 
         // ボリュームボタンでメディアの音量調整に設定
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        // ボタンオブジェクト取得
+        Button btnRec = (Button) findViewById(R.id.record);
+        Button btnPlay = (Button) findViewById(R.id.play);
+        Button btnPause = (Button) findViewById(R.id.pause);
+        Button btnStop = (Button) findViewById(R.id.stop);
+
+        // タグ・リスナー登録
+        btnRec.setTag(REC);
+        btnRec.setOnClickListener(new BtnClickListener());
+        btnPlay.setTag(PLAY);
+        btnPlay.setOnClickListener(new BtnClickListener());
+        btnPause.setTag(PAUSE);
+        btnPause.setOnClickListener(new BtnClickListener());
+        btnStop.setTag(STOP);
+        btnStop.setOnClickListener(new BtnClickListener());
+    }
+
+    public class BtnClickListener implements OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            // タグ取得
+            String tag = (String) v.getTag();
+
+            // タグにより機能振り分け
+            switch (tag) {
+            // 録音
+            case REC:
+                Toast.makeText(PianoTouchSampleActivity.this, "録音を開始します",
+                    Toast.LENGTH_SHORT).show();
+                break;
+            // 再生
+            case PLAY:
+                Toast.makeText(PianoTouchSampleActivity.this, "再生します",
+                    Toast.LENGTH_SHORT).show();
+                break;
+            // 一時停止
+            case PAUSE:
+                Toast.makeText(PianoTouchSampleActivity.this, "一時停止します",
+                    Toast.LENGTH_SHORT).show();
+                break;
+            // 停止
+            case STOP:
+                Toast.makeText(PianoTouchSampleActivity.this, "停止します",
+                    Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
     }
 
     // onDestroyメソッド(アクティビティ破棄イベント)
